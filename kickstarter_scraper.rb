e58872f3ebdea7627b726473fe7c62c4dad64f2d
Tracy Ngo <tngo1206@gmail.com>
2019-03-28 19:41:33 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,6 +8,8 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   
   project = {}
+  
+  kickstarter.css
   # projects: kickstarter.css("li.project.grid_4")
   # title: project.css("h2.bbcard_name strong a").text
   # image link: project.css("div.project-thumbnail a img").attribute("src").value
