a8646c25b06c7bd256be68ea0e750d4cd20c82ac
Tracy Ngo <tngo1206@gmail.com>
2019-03-28 19:30:54 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,8 +6,8 @@ def create_project_hash
   # write your code here
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  binding.pry
+  
   
   # projects: kickstarter.css("li.project.grid_4")
 end
-
+binding.pry
