4e16b9ebcacb3cc3cb7175dafdaceb53855401c4
Tracy Ngo <tngo1206@gmail.com>
2019-03-28 19:45:43 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -19,11 +19,5 @@ def create_project_hash
   
   projects
   
-  # projects: kickstarter.css("li.project.grid_4")
-  # title: project.css("h2.bbcard_name strong a").text
-  # image link: project.css("div.project-thumbnail a img").attribute("src").value
-  # description: project.css("p.bbcard_blurb").text
-  # location: project.css(".location-name").text
-  # percent funded: project.css(ul.project-stats li.first.funded strong).text.gsub("%", "").to_i
 end
 
